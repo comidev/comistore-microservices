493aa4fa25f6d5ecfddc5ee45b7916a16b396846
Omar Miranda <comidev.contacto@gmail.com>
2022-07-18 17:35:18 -0500

    agregando Eureka

--- services/auth/src/main/java/comidev/AuthApp.java
+++ services/auth/src/main/java/comidev/AuthApp.java
@@ -1,11 +1,13 @@
 package comidev;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
 public class AuthApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(AuthApp.class, args);
     }
 }
--- services/customer/src/main/java/comidev/CustomerApp.java
+++ services/customer/src/main/java/comidev/CustomerApp.java
@@ -1,11 +1,13 @@
 package comidev;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
 public class CustomerApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(CustomerApp.class, args);
     }
 }
